07d0d39c2fb5dfb4ed65faf08f851f66be287437
simple1017 <simple01281017@gmail.com>
2023-01-20 16:53:43 +0800

    Init

---
+++ src/pages/OrderPage.tsx
@@ -0,0 +1,14 @@
+const OrderPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <ul className="steps">
+        <li className="step-primary step">Register</li>
+        <li className="step-primary step">Choose plan</li>
+        <li className="step">Purchase</li>
+        <li className="step">Receive Product</li>
+      </ul>
+    </div>
+  );
+};
+
+export default OrderPage;
