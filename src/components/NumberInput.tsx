0b74af72984d14d0d6afc49fe417856a8e5a148d
simple1017 <simple01281017@gmail.com>
2023-01-21 19:56:16 +0800

    feat: Add NumberInput component for pages

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,25 @@
+interface InputType {
+  description: string;
+  value: number;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+}
+
+const NumberInput = ({ description, value, onChange }: InputType) => {
+  return (
+    <div className="w-60 space-y-3">
+      <label htmlFor="numberInput">Please Enter {description}</label>
+      <input
+        id="numberInput"
+        type="number"
+        placeholder="Enter here"
+        className="input-bordered input w-full max-w-xs"
+        min="1"
+        max="10"
+        defaultValue={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default NumberInput;
