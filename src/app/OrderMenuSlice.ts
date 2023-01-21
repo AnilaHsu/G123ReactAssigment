42673d15179e674338ca12a16288a2852a2d09ae
simple1017 <simple01281017@gmail.com>
2023-01-21 21:54:43 +0800

    feat: update the state base on user select
    - add data to redux state as dishData
    - update the restaurants base on selectedMeal
    - add the dishes base  on selectedMeal and selectedRestaurants
    - add action creator of selectDish and selectServingsNumber

--- src/app/OrderMenuSlice.ts
+++ src/app/OrderMenuSlice.ts
@@ -1,24 +1,35 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import data from "../data/dishes.json";
 
-const restaurants: string[] = Array.from(
-  new Set(data.dishes.map((dish) => dish.restaurant))
-);
+interface dishesData {
+  id: number;
+  name: string;
+  restaurant: string;
+  availableMeals: string[];
+}
 
 interface OrderMenuState {
+  dishesData: dishesData[];
   selectedMeal: string;
   availableMeals: string[];
   peopleNumber: number;
   selectedRestaurant: string;
   restaurants: string[];
+  selectedDish: string;
+  dishes: string[];
+  servingsNumber: number;
 }
 
 const initialState: OrderMenuState = {
+  dishesData: data.dishes,
   selectedMeal: "",
   availableMeals: ["breakfast", "lunch", "dinner"],
   peopleNumber: 1,
   selectedRestaurant: "",
-  restaurants: restaurants,
+  restaurants: [],
+  selectedDish: "",
+  dishes: [],
+  servingsNumber: 1,
 };
 
 export const OrderMenuSlice = createSlice({
@@ -27,16 +38,48 @@ export const OrderMenuSlice = createSlice({
   reducers: {
     selectMeal: (state, action: PayloadAction<string>) => {
       state.selectedMeal = action.payload;
+      state.restaurants = Array.from(
+        new Set(
+          state.dishesData
+            .filter((dish) => {
+              return dish.availableMeals.includes(action.payload);
+            })
+            .map((dish) => dish.restaurant)
+        )
+      );
     },
     selectPeopleNumber: (state, action: PayloadAction<number>) => {
       state.peopleNumber = action.payload;
     },
     selectRestaurant: (state, action: PayloadAction<string>) => {
       state.selectedRestaurant = action.payload;
+      state.dishes = Array.from(
+        new Set(
+          state.dishesData
+            .filter((dish) => {
+              return dish.availableMeals.includes(state.selectedMeal);
+            })
+            .filter((dish) => {
+              return dish.restaurant.includes(action.payload);
+            })
+            .map((dish) => dish.name)
+        )
+      );
+    },
+    selectDish: (state, action: PayloadAction<string>) => {
+      state.selectedDish = action.payload;
+    },
+    selectServingsNumber: (state, action: PayloadAction<number>) => {
+      state.servingsNumber = action.payload;
     },
   },
 });
 
-export const { selectMeal, selectPeopleNumber, selectRestaurant } =
-  OrderMenuSlice.actions;
+export const {
+  selectMeal,
+  selectPeopleNumber,
+  selectRestaurant,
+  selectDish,
+  selectServingsNumber,
+} = OrderMenuSlice.actions;
 export default OrderMenuSlice.reducer;
